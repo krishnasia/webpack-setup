aa33e0f02539a16c340396c91fa413de006275be
krishna <krishnasiva019@gmail.com>
2022-12-01 12:29:36 +0530

    webpack changes done for site loading using npm start

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./App.css";
+export const App = () => {
+  return (
+    <div>
+        <h1 className='headingOne'>React Application without npx-create-react-app using webpack.config.js</h1>
+    </div>
+  )
+}
